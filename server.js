6d8e3400ad3fd947db8d201eb5c3c7b76aecebf2
sayedalali1 <sayedalali1@gmail.com>
2024-07-23 09:52:34 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const dotenv = require("dotenv");
+
+dotenv.config();
+const express = require("express");
+const app = express();
+
+require('./config/database');
+const methodOverride = require("method-override");
+const morgan = require("morgan");
+
+// Set the port from environment variable or default to 3000
+const port = process.env.PORT ? process.env.PORT : "3000";
+
+// MIDDLEWARE
+
+
+// Middleware to parse URL-encoded data from forms
+app.use(express.urlencoded({ extended: false }));
+// Middleware for using HTTP verbs such as PUT or DELETE
+app.use(methodOverride("_method"));
+// Morgan for logging HTTP requests
+app.use(morgan('dev'));
+
+// ROUTES
+
+app.listen(port, () => {
+  console.log(`The express app is ready on port ${port}!`);
+});
